8946e36ea3e236a3d980fb677d042e03160e78b9
jian_liu <jianliu0616@gmail.com>
2022-10-25 23:18:24 +0800

    feat(create): update transaction implementation

--- pkg/storage/mysql/urls.go
+++ pkg/storage/mysql/urls.go
@@ -1,7 +1,8 @@
 package mysql
 
 import (
-	"errors"
+	"database/sql"
+	"fmt"
 	"time"
 	"tinyurl/util"
 
@@ -19,22 +20,30 @@ type Url struct {
 }
 
 func CreateUrl(data *Url, isCustomAlias bool) error {
+	txOptions := &sql.TxOptions{
+		Isolation: sql.LevelRepeatableRead,
+		ReadOnly:  false,
+	}
+
 	alias := Url{}
 
 	return instance.Transaction(func(tx *gorm.DB) error {
+
 		// 檢查資料庫是否已經存在相同的短網址
-		if err := tx.Table(tbUrls).Select("origin").Where("tiny = ?", data.Tiny).First(&alias).Error; err != nil {
-			if !errors.Is(err, gorm.ErrRecordNotFound) {
-				return err
-			}
-			// 短網址未發生碰撞, 直接將短網址的 meatadata 寫入資料庫
-			// 若本來就存在相同的原始網址, 直接覆蓋新的 meatadata 即可
-			return tx.Table(tbUrls).Where(Url{Origin: data.Origin}).FirstOrCreate(&data).Error
+		if result := tx.Table(tbUrls).Where("tiny = ? OR origin = ?", data.Tiny, data.Origin).Limit(1).Find(&alias); result.RowsAffected == 0 {
+			// 查無資料, 直接寫入
+			return tx.Table(tbUrls).Create(&data).Error
 		}
 
-		// 已經存在相同資料, 不處理也不增加 expired time
+		// 已經存在相同資料
 		if alias.Origin == data.Origin {
-			return nil
+			// 未預期的錯誤: 相同的原始網址, 但短網址不同
+			if alias.Tiny != data.Tiny {
+				return fmt.Errorf("unexpected error: Req:%v, DB:%v", data, alias)
+			}
+
+			// 以後可以考慮是否要延長 expiration time
+			return gorm.ErrInvalidData
 		}
 
 		// 短網址發生碰撞, 且短網址是用戶自定義
@@ -45,8 +54,9 @@ func CreateUrl(data *Url, isCustomAlias bool) error {
 		// 將 timestamp 作為後綴詞加入短網址
 		suffix := util.Base10ToBase62(uint64(time.Now().UnixMilli()))
 		data.Tiny += suffix
-		return tx.Table(tbUrls).Where(Url{Origin: data.Origin}).FirstOrCreate(&data).Error
-	})
+		return tx.Table(tbUrls).Create(&data).Error
+
+	}, txOptions)
 }
 
 func GetUrl(tiny_url string) (res Url, err error) {
